5265eef7c19683739046baf1a0d0303907358958
Sirwan <sir1afifi@gmail.com>
2020-06-24 20:32:24 +0430

    Add location

--- App.js
+++ App.js
@@ -1,19 +1,49 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import * as Location from "expo-location";
+import MapView, { Marker } from "react-native-maps";
 
 export default function App() {
+  const [location, setLocation] = useState();
+
+  const getLocation = async () => {
+    const { granted } = await Location.requestPermissionsAsync();
+    if (!granted) return;
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getLastKnownPositionAsync();
+    setLocation({
+      latitude,
+      longitude,
+      latitudeDelta: 0.0922,
+      longitudeDelta: 0.0421,
+    });
+  };
+
+  useEffect(() => {
+    getLocation();
+  }, []);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
+    <MapView
+      provider="google"
+      showsUserLocation
+      showsCompass
+      showsMyLocationButton
+      style={styles.container}
+      initialRegion={location}
+    >
+      <Marker coordinate={location} />
+    </MapView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  mapStyle: {},
 });
